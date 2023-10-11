7415c289013ff66f820b8a520d2c6e14d74bd195
Sota-Yamanaka <y.sota.tf0429@icloud.com>
2023-10-11 19:17:58 +0900

    編集機能追加完了

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ Route::controller(PostController::class)->middleware(['auth'])->group(function()
     Route::get('/posts/{id}/detail','detail')->name('posts.detail');//練習メニュー詳細
     Route::get('/posts', 'profile')->name('profile');
     Route::get('/practice', 'practice')->name('practice');//練習メニュー一覧
-    Route::get('/posts/create','create')->name('posts.create');//メニュー登録
-    Route::post('/posts/{id}/store','store')->name('posts.store');//メニュー登録処理
+    Route::get('/posts/create/{id?}','create')->name('posts.create');//メニュー登録
+    Route::post('/posts/store/{id?}','store')->name('posts.store');//メニュー登録処理
     Route::get('/posts/practice', 'practice')->name('posts.practice');
     Route::get('/posts/{id}/edit','edit')->name('posts.edit');//練習メニュー編集
     Route::post('/posts/{id}/delete','delete')->name('posts.delete');//削除処理
